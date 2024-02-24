5d6d62a9c1aa4940fd3e753064afff242bbc3540
reinheimermat <mreinheimer0@gmail.com>
2024-02-24 13:05:45 -0300

    feat: add products listing.

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -1,9 +1,20 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <div>
-      <h1>Next Ecommerce</h1>
-    </div>
+    <section className='max-w-7xl mx-auto pt-8 px-8 xl:px-0'>
+      <article className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        <div className="bg-white">
+          <h2 className="text-2xl font-bold mb-4">Next E-Commerce 13</h2>
+        </div>
+        <div className="bg-white">
+          <h2 className="text-2xl font-bold mb-4">Next E-Commerce 13</h2>
+        </div>
+        <div className="bg-white">
+          <h2 className="text-2xl font-bold mb-4">Next E-Commerce 13</h2>
+        </div>
+        <div className="bg-white">
+          <h2 className="text-2xl font-bold mb-4">Next E-Commerce 13</h2>
+        </div>
+      </article>
+    </section>
   )
 }
