873413eb46d53d170a657ffe1789ac12e0e8f958
reinheimermat <mreinheimer0@gmail.com>
2024-02-24 13:17:58 -0300

    feat: defines typing for the products.

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -1,19 +1,24 @@
-export default function Home() {
+import { ProductType } from "@/types/productType"
+
+async function getProducts() {
+  const response = await fetch('https://fakestoreapi.com/products')
+
+  if (!response.ok) {
+    throw new Error('Falha ao buscar os produtos')
+  }
+  return response.json()
+}
+export default async function Home() {
+  const products = await getProducts()
+
   return (
     <section className='max-w-7xl mx-auto pt-8 px-8 xl:px-0'>
       <article className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
-        <div className="bg-white">
-          <h2 className="text-2xl font-bold mb-4">Next E-Commerce 13</h2>
-        </div>
-        <div className="bg-white">
-          <h2 className="text-2xl font-bold mb-4">Next E-Commerce 13</h2>
-        </div>
-        <div className="bg-white">
-          <h2 className="text-2xl font-bold mb-4">Next E-Commerce 13</h2>
-        </div>
-        <div className="bg-white">
-          <h2 className="text-2xl font-bold mb-4">Next E-Commerce 13</h2>
-        </div>
+        {products.map((product: ProductType) => {
+          <aside key={product.id}>
+            {product.title}
+          </aside>
+        })}
       </article>
     </section>
   )
