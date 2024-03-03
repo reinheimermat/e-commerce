715f8194ac8e3e7fb4c3b34aa220f16210069502
reinheimermat <mreinheimer0@gmail.com>
2024-03-03 09:36:36 -0300

    feat: create checkout component.

--- project/src/app/components/cartDrawer.tsx
+++ project/src/app/components/cartDrawer.tsx
@@ -4,6 +4,7 @@ import { formatPrice } from "@/lib/utils";
 import { useCartStore } from "@/store";
 import Image from "next/image";
 import { CheckoutButton } from "./checkoutButton";
+import { Checkout } from "./checkout";
 
 export function CartDrawer() {
   const useStore = useCartStore();
@@ -28,41 +29,47 @@ export function CartDrawer() {
           Voltar para a loja
         </button>
         <div className="border-t border-gray-400 my-4"></div>
-        {useStore.cart.map((item) => (
-          <aside key={item.id} className="flex gap-4 py-4">
-            <Image
-              src={item.image}
-              alt={item.name}
-              width={120}
-              height={120}
-              className="object-cover w-24"
-            />
-            <div>
-              <h2 className="w-42 truncate">{item.name}</h2>
-              <h2>Quantidade: {item.quantity}</h2>
-              <p className="text-teal-600 text-sm font-bold">
-                {formatPrice(item.price)}
-              </p>
-              <button
-                onClick={() => useStore.addProduct(item)}
-                className="py-1 px-2 border rounded-md mt-2 text-sm mr-1"
-              >
-                Adicionar
-              </button>
-              <button
-                onClick={() => useStore.removeProduct(item)}
-                className="py-1 px-2 border rounded-md mt-2 text-sm"
-              >
-                Remover
-              </button>
-            </div>
-          </aside>
-        ))}
+        {useStore.onCheckout === "checkout" && (
+          <>
+            {useStore.cart.map((item) => (
+              <aside key={item.id} className="flex gap-4 py-4">
+                <Image
+                  src={item.image}
+                  alt={item.name}
+                  width={120}
+                  height={120}
+                  className="object-cover w-24"
+                />
+                <div>
+                  <h2 className="w-42 truncate">{item.name}</h2>
+                  <h2>Quantidade: {item.quantity}</h2>
+                  <p className="text-teal-600 text-sm font-bold">
+                    {formatPrice(item.price)}
+                  </p>
+                  <button
+                    onClick={() => useStore.addProduct(item)}
+                    className="py-1 px-2 border rounded-md mt-2 text-sm mr-1"
+                  >
+                    Adicionar
+                  </button>
+                  <button
+                    onClick={() => useStore.removeProduct(item)}
+                    className="py-1 px-2 border rounded-md mt-2 text-sm"
+                  >
+                    Remover
+                  </button>
+                </div>
+              </aside>
+            ))}
+          </>
+        )}
 
         {/* Renderização condicional */}
-        {useStore.cart.length > 0 && useStore.onCheckout === 'cart' && (
+        {useStore.cart.length > 0 && useStore.onCheckout === "cart" && (
           <CheckoutButton totalPrice={totalPrice} />
         )}
+
+        {useStore.onCheckout === "checkout" && <Checkout />}
       </div>
     </article>
   );
